84d5068ddcff7cf2bc8f44f3133eb6a7cb37c214
hexa_fred <fredbat95@gmail.com>
2023-02-11 16:36:49 +0100

    Add Bouncing

--- src/Ball.java
+++ src/Ball.java
@@ -4,7 +4,6 @@ public class Ball {
 
     private double vy = Constants.BALL_VELOCITY_X;
     private double vx = Constants.BALL_VELOCITY_Y;
-    //velocity x, y
 
     public Ball(Rect rect, Rect leftPaddle, Rect rightPaddle) {
         this.rect = rect;
@@ -12,32 +11,50 @@ public class Ball {
         this.rightPaddle = rightPaddle;
     }
 
+    public double calculateNewVelocityAngle(Rect paddle) {
+        double relativeIntersectY = (paddle.y + (paddle.height / 2.0)) - (this.rect.y + (this.rect.height / 2.0));
+        double normalIntersectY = relativeIntersectY / (paddle.height /2.0);
+        double theta = normalIntersectY * Constants.MAX_ANGLE;
+
+        return Math.toRadians(theta);
+    }
+
     public void update(double dt) {
-        if (vx < 0) {
+        if (vx < 0.0) {
             if (this.rect.x <= this.leftPaddle.x + this.leftPaddle.width && this.rect.x + this.rect.width >= this.leftPaddle.x &&
                 this.rect.y >= this.leftPaddle.y && this.rect.y <= this.leftPaddle.y + this.leftPaddle.height) {
-                this.vx *= -1;
-                this.vy *= -1;
+                double theta = calculateNewVelocityAngle(leftPaddle);
+                double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
+
+                double oldSign = Math.signum(vx);
+                this.vx = newVx * (-1.0 * oldSign);
+                this.vy = newVy;
             } else if (this.rect.x + this.rect.width < this.leftPaddle.x){
                 System.out.println("You lost");
             }
-        } else if (vx > 0) {
+        } else if (vx > 0.0) {
             if (this.rect.x + this.rect.width >= this.rightPaddle.x && this.rect.x <= this.rightPaddle.x + this.rightPaddle.width &&
                     this.rect.y >= this.rightPaddle.y && this.rect.y <= this.rightPaddle.y + this.rightPaddle.height) {
-                this.vx *= -1;
-                this.vy *= -1;
+                double theta = calculateNewVelocityAngle(rightPaddle);
+                double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                double newVy = (Math.sin(theta)) * Constants.BALL_SPEED;
+
+                double oldSign = Math.signum(vx);
+                this.vx = newVx * (-1.0 * oldSign);
+                this.vy = newVy;
             } else if (this.rect.x + this.rect.width > this.rightPaddle.x + this.rightPaddle.width ) {
                 System.out.println("Ai has lost a point");
             }
         }
 
-        if (vy > 0) {
+        if (vy > 0.0) {
             if(this.rect.y + this.rect.height > Constants.SCREEN_HEIGHT) {
-                this.vy *= -1;
+                this.vy *= -1.0;
             }
-        } else if (vy < 0) {
+        } else if (vy < 0.0) {
             if (this.rect.y < Constants.TOOLBAR_HEIGHT) {
-                this.vy *= -1;
+                this.vy *= -1.0;
             }
         }
 
--- src/Constants.java
+++ src/Constants.java
@@ -5,16 +5,19 @@ public class Constants {
     public static final int SCREEN_HEIGHT = 600;
     public static final String SCREEN_TITLE = "Pong";
 
-    public static final double PADDLE_WIDTH = 10;
-    public static final double PADDLE_HEIGHT = 100;
+    public static final double PADDLE_WIDTH = 10.0;
+    public static final double PADDLE_HEIGHT = 100.0;
     public static final Color PADDLE_COLOR = Color.white;
-    public static final double BALL_WIDTH = 10;
-    public static final double HZ_PADDING = 40;
-    public static final double PADDLE_SPEED = 300;
+    public static final double BALL_WIDTH = 10.0;
+    public static final double HZ_PADDING = 40.0;
+    public static final double PADDLE_SPEED = 300.0;
+    public static final double BALL_SPEED = 190.0;
     public static final double PADDLE_START_Y = (Constants.SCREEN_HEIGHT/2.0)-(Constants.PADDLE_HEIGHT/2.0);
 
-    public static final double BALL_VELOCITY_X = 200.0;
-    public static final double BALL_VELOCITY_Y = -100.0;
+    public static final double BALL_VELOCITY_X = 190.0;
+    public static final double BALL_VELOCITY_Y = -190.0;
+
+    public static final double MAX_ANGLE = 45.0;
 
     public static double TOOLBAR_HEIGHT;
     public static double INSETS_BOTTOM;
